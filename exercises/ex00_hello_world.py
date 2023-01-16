42c1efc0ac2b4b78bd94de08f51a7aa4a7035f21
Zoe Schubert <122545230+zoeschubert@users.noreply.github.com>
2023-01-16 14:21:06 -0500

    Finished Exercise 00!

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,7 +1,1 @@
-"""My first program for COMP110."""
-_author_ = "730466642"
-<<<<<<< HEAD
-print("Hello, wonderful world.")
-=======
-print("Hello, wonderful world.")
->>>>>>> a58378a (Finished Exercise 00!)
+hello
