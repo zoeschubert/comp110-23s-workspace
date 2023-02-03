949b16c3f04fac9af6ff615027759a9adc8ab679
Zoe Schubert <122545230+zoeschubert@users.noreply.github.com>
2023-02-03 18:35:44 -0500

    Progress on Exercise 02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,28 +1,28 @@
 """EX02 - One-Shot Wordle - Loops!"""
-__author__ : "730466642"
+__author__ = "730466642"
 
 secret_word: str = "python"
 guess: str = input("What is your 6-letter guess? ")
-length_secret: str = len(secret_word)
+length_secret: int = len(secret_word)
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-i : int = 0
+i: int = 0
 emoji: str = ""
 
 while len(guess) != len(secret_word):
-#check if guessed word is 6 letters long
+    # check if guessed word is 6 letters long
     guess = input(f"That was not {length_secret} letters! Try again: ")
 
 while i < len(secret_word):
-#test to see if current index of guessed word is equal to the same index of secret word
+    # test to see if current index of guessed word is equal to the same index of secret word
     if guess[i] == secret_word[i]:
         emoji = emoji + GREEN_BOX
     else: 
         exists: bool = False
         alt_idx: int = 0
         while (exists is False) and (alt_idx < len(secret_word)):
-        #test to see if letter exists in word
+            # test to see if letter exists in word
             if guess[i] != secret_word[alt_idx]:
                 alt_idx = alt_idx + 1
             else:
