b2becc8fe58be09a8107fe159114ba2908e3b260
Zoe Schubert <122545230+zoeschubert@users.noreply.github.com>
2023-02-27 12:43:24 -0500

    Progress on exercise 04

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,42 @@
+"""List Utility Functions"""
+__author__ : "730466642"
+
+def all(x: list[int], y: int)->bool:
+    length: int = len(x)
+    i: int = 0
+    while (length > i):
+        #The list is not empty and the index does not exceed the length of the list
+        if x[i] == y:
+            i+=1
+        else:
+            #The integer does not match all number in the list
+            return False
+    return True
+
+
+def max(input: list[int])->int:
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    max: int = input[0]
+    i: int = 0
+    while (i<len(input)-1):
+        if max<input[i+1]:
+            max = input[i+1]
+        i+=1
+    return max
+            
+        
+
+def is_equal(list1: list[int], list2: list[int])->bool:
+    i: int = 0
+    length1: int = len(list1)
+    length2: int = len(list2)
+    while (i<length1 and i<length2):
+        if list1[i] == list2[i] and length1 == length2: 
+            i+=1
+        else: 
+            return False
+    return True
+
+
+
