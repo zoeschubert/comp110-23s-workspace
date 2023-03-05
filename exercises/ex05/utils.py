10b252050f30f94781ee33fc8b136a79b1e2d85a
Zoe Schubert <122545230+zoeschubert@users.noreply.github.com>
2023-03-05 13:35:32 -0500

    Progress on EX05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,16 +1,18 @@
-"""EX05 - 'list' Utility Functions"""
+"""EX05 - 'list' Utility Functions."""
 __author__ = "730466642"
 
+
 def only_evens(input: list[int]) -> list[int]: 
-    """given a list of numbers, return only the even numbers"""
+    """Given a list of numbers, return only the even numbers."""
     evens: list[int] = []
     for number in input:
         if number % 2 == 0:
             evens.append(number)
     return evens
 
+
 def concat(list1: list[int], list2: list[int]) -> list[int]:
-    """given two lists, return the first list followed by the second"""
+    """Given two lists, return the first list followed by the second."""
     new_list: list[int] = []
     for item in list1:
         new_list.append(item)
@@ -18,8 +20,9 @@ def concat(list1: list[int], list2: list[int]) -> list[int]:
         new_list.append(item)
     return new_list
 
+
 def sub(input: list[int], start: int, end: int) -> list[int]:
-    """select a section of a list"""
+    """Select a section of a list."""
     length: int = len(input)
     new_list: list[int] = []
     if start < 0:
